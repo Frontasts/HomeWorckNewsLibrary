28b00536f4020530819337b0605dd58497a442ce
Frontasts <steamfronts0110@gmail.com>
2026-07-13 02:16:23 +0300

    Добавленно поле и конструктор

--- HomeWorckNewsLibrary/Author.cs
+++ HomeWorckNewsLibrary/Author.cs
@@ -8,5 +8,11 @@ namespace HomeWorckNewsLibrary
 {
     public class Author
     {
+        private string _name;
+
+        public Author(string name)
+        {
+            _name = name;
+        }
     }
 }
